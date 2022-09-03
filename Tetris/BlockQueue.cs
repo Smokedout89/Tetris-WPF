6886631c9a53dc84d19459464b06abb356ffa3fa
Smokedout89 <smokedout8989@gmail.com>
2022-09-03 16:58:32 +0300

    Initial commit

---
+++ Tetris/BlockQueue.cs
@@ -0,0 +1,43 @@
+﻿namespace Tetris;
+
+using System;
+
+public class BlockQueue
+{
+    private readonly Random random = new Random();
+
+    private readonly Block[] blocks = new Block[]
+    {
+        new IBlock(),
+        new JBlock(),
+        new LBlock(),
+        new OBlock(),
+        new SBlock(),
+        new TBlock(),
+        new ZBlock()
+    };
+
+    public BlockQueue()
+    {
+        NextBlock = RandomBlock();
+    }
+
+    public Block NextBlock { get; private set; }
+
+    private Block RandomBlock()
+    {
+        return blocks[random.Next(blocks.Length)];
+    }
+
+    public Block GetAndUpdate()
+    {
+        Block block = NextBlock;
+
+        do
+        {
+            NextBlock = RandomBlock();
+        } while (block.Id == NextBlock.Id);
+
+        return block;
+    }
+}
